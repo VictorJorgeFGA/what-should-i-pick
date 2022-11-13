22bb626c985bb1bcd989a6f6f9af660e7d07d183
VictorJorgeFGA <victor.eng.unb@gmail.com>
2022-11-13 12:40:36 -0300

    Fixes tests

--- test/models/champion_test.rb
+++ test/models/champion_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class ChampionTest < ActiveSupport::TestCase
   test 'should not allow duplicating champions by riot key' do
-    Champion.create!({key: 123})
+    Champion.create!({ key: 123 })
 
     champ = Champion.new(key: 123)
 
-    refute champ.valid?
+    assert_not champ.valid?
     assert_equal :taken, champ.errors.details[:key].first[:error]
   end
 end
