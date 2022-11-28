b4dd3802cf6549f256f75460b72b5a2d4528bd08
Victor Gonçalves <victor.eng.unb@gmail.com>
2022-11-28 00:54:56 -0300

    Change some styles

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,20 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-// Create the observer, same as before:
-const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('typewriter');
-            return;
-        }
-
-            entry.target.classList.remove('typewriter');
-        });
-    });
-
-    var texts = document.getElementsByClassName('typewriter');
-    for (let item of texts) {
-    observer.observe(item);
-}
